b34d1891ebb1cd22d8eeaca4fe1e2198320a3689
minot michael <nexspro@ik.me>
2025-03-05 15:29:35 +0100

    exo 2

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,26 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+require "faker"
+
+puts "Delete old Data"
+Restaurant.destroy_all
+
+CATEGORIES = %w[chinese italian japanese french belgian philipine]
+
+puts "Creating Restaurants"
+10.times do
+  restaurant = Restaurant.create!(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.full_address,
+    phone_number: Faker::PhoneNumber.phone_number,
+    category: CATEGORIES.sample
+  )
+
+  rand(1..10).times do
+    Review.create!(
+      content: Faker::Restaurant.review,
+      rating: rand(0..5),
+      restaurant: restaurant
+    )
+  end
+end
+
+puts "Tu as crée #{Restaurant.count} restaurants et #{Review.count} reviews"
